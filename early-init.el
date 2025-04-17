64672244537f978a0e842df5835caa87bf491493
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2025-04-17 06:34:20 +0300

    make early init check display-graphic-p for some settings

--- early-init.el
+++ early-init.el
@@ -26,29 +26,28 @@
 ;; Vertical window divider
 (setq window-divider-default-right-width 4
       window-divider-default-places 'right-only)
-;; (window-divider-mode 1)
+(window-divider-mode 1)
 
 ;; ;; No ugly button for checkboxes
 ;; (setq widget-image-enable nil)
 
+
+;; Only disable GUI elements if running in a graphical display
 (when (display-graphic-p)
-  (progn
-    (menu-bar-mode -1)
-    (tool-bar-mode -1)
-    (scroll-bar-mode -1)))
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1))
 
 (add-hook 'after-make-frame-functions
           (lambda (frame)
-            (select-frame frame)
-            (if (display-graphic-p)
-                (progn
-                  ;; (set-frame-size frame 93 50)
-                  ;; (set-frame-position frame 0 15)
-                  ;; (set-window-margins nil 2)
-                  (scroll-bar-mode -1)
-                  (menu-bar-mode -1)
-                  (tool-bar-mode -1)
-                  ))))
+            (when (display-graphic-p frame)
+              (with-selected-frame frame
+                ;; (set-frame-size frame 93 50)
+                ;; (set-frame-position frame 0 15)
+                ;; (set-window-margins nil 2)
+                (scroll-bar-mode -1)
+                (menu-bar-mode -1)
+                (tool-bar-mode -1)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; some minor ui and font settings
 (set-window-margins nil 2)
@@ -67,28 +66,30 @@
 ;; (set-face-attribute 'default nil :font "Droid Sans Mono")
 ;; (set-fontset-font t 'emoji (font-spec :family "Noto Color Emoji") nil 'prepend)
 
+(defun yae-set-font (&optional frame)
+  (when (display-graphic-p (or frame (selected-frame)))
+    (with-selected-frame (or frame (selected-frame))
+      ;; (set-frame-font "JetBrainsMono Nerd Font")
+      (set-frame-font "LiterationMono Nerd Font")
+      ;; (set-frame-font "CommitMono Nerd Font")
+      (set-face-attribute 'default nil :height 110))))
+
 (when (display-graphic-p)
-  (defun yae-set-font (&optional frame)
-    (when frame (select-frame frame))
-    (set-frame-font "LiterationMono Nerd Font")
-    ;; (set-frame-font "CommitMono Nerd Font")
-	)
-  (yae-set-font)
-  (set-face-attribute 'default nil :height 110)
-  (add-hook 'after-make-frame-functions 'yae-set-font)
+  (yae-set-font))
 
+(add-hook 'after-make-frame-functions 'yae-set-font)
 
-  ;; (setq default-frame-alist '((font . "JetBrainsMono Nerd Font 9"))) ;;; set default font for emacs --daemon / emacsclient
+;; Fontset settings – only safe to apply in GUI
+(when (display-graphic-p)
+  ;; (setq default-frame-alist '((font . "JetBrainsMono Nerd Font 9")))
   ;; (add-to-list 'default-frame-alist (cons 'font  my-default-font))
   (set-fontset-font t nil "Dejavu Sans Mono" nil 'append)
   (set-fontset-font t 'ethiopic "Noto Sans Ethiopic")
   ;; (set-fontset-font t 'thai "Leelawadee UI" nil 'append)
-  )
-
-;; (setq w32-enable-synthesized-fonts t) ;;The w32-enable-synthesized-fonts variable is obsolete starting from Emacs 24.4, as Emacs no longer has this limitation.
-;; (set-face-font 'italic "-*-Courier New-normal-i-*-*-11-*-*-*-c-*-iso8859-1")
-;; (set-face-font 'bold-italic "-*-Courier New-bold-i-*-*-11-*-*-*-c-*-iso8859-1")
-
+  ;; (setq w32-enable-synthesized-fonts t) ;;The w32-enable-synthesized-fonts variable is obsolete starting from Emacs 24.4, as Emacs no longer has this limitation.
+  ;; (set-face-font 'italic "-*-Courier New-normal-i-*-*-11-*-*-*-c-*-iso8859-1")
+  ;; (set-face-font 'bold-italic "-*-Courier New-bold-i-*-*-11-*-*-*-c-*-iso8859-1")
+)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
