6dbf27ee4a242f12ce3cef7f2223abc861166b5a
yilkalargaw <yilkalargawworkneh@gmail.com>
2025-06-08 16:10:58 +0000

    have a better bootstrapping mechanism

--- early-init.el
+++ early-init.el
@@ -8,7 +8,21 @@
 
 ;;; Code:
 
-(require 'org)
-(org-babel-tangle-file (expand-file-name "early-init.org" user-emacs-directory))
+;; Minimal early-init.el - JUST for bootstrapping
+(setq package-enable-at-startup nil)  ; Disable package.el until init
+
+;; Compile config.org if needed
+(let ((org-file (expand-file-name "early-init.org" user-emacs-directory)))
+  (when (or (not (file-exists-p (expand-file-name "early-init.elc" user-emacs-directory)))
+            (file-newer-than-file-p org-file
+                                   (expand-file-name "early-init.elc" user-emacs-directory)))
+    (require 'org)
+    (require 'ox)
+    (org-babel-tangle-file org-file
+                          (expand-file-name "early-init.el" user-emacs-directory))
+    (byte-compile-file (expand-file-name "early-init.el" user-emacs-directory))))
+
+;; Load the compiled version
+(load (expand-file-name "early-init.elc" user-emacs-directory) t t)
 
 ;;; early-init.el ends here
